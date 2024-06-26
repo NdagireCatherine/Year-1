c4bea502dce387f3ffc8d86c8492881ca3ac859c
Tronnie123 <tronellahn@gmail.com>
2024-06-26 10:22:41 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,40 @@
+#number_1
+birth_yr=int(input("Enter your year:"))
+age=2024-birth_yr
+print(f"You are {age} years old.")
+
+#number_2
+num_A=int(input("Enter number A:"))
+num_B=int(input("Enter number B:"))
+sum=num_A+num_B
+product=num_A*num_B
+print(f"Sum of these numbers is {sum} and the product of these numbers is {product}.")
+
+
+#number_3
+num=int(input("Enter a value:"))
+square=num*num
+print(f"Number is {num} and its square is {square}")
+
+
+#number_4
+num_a=int(input("Enter number A:"))
+num_b=int(input("Enter number B:"))
+sum_of_numbers=num_a+num_b
+product_of_numbers=num_a*num_b
+print(f"Sum of {num_a} and {num_b} is {sum_of_numbers} and product of {num_a} and {num_b} is {product_of_numbers}")
+
+
+#number_5
+lgth=int(input("Enter length:"))
+breadth=int(input("Enter breadth"))
+perimeter=lgth+lgth+breadth+breadth
+area=lgth*breadth
+print(f"Perimeter of rectangle is {perimeter} and area of rectangle is {area}")
+
+
+
+#number_6
+cel_value=int(input("Enter celsius value:"))
+fahr_value=(1.8*cel_value)+32
+print(f"Fahrenheit value is {fahr_value}")
